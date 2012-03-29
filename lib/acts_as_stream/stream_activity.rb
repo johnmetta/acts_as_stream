95c77d29840fc3505aba1ce0f969b937539b7a4a
John Metta <john@johnmetta.com>
2012-03-29 07:42:16 -0700

    don't catch errors in StreamActivity parsing

--- lib/acts_as_stream/stream_activity.rb
+++ lib/acts_as_stream/stream_activity.rb
@@ -10,15 +10,15 @@ module ActsAsStream
 
       [:who, :object].each do |opt|
         #unless we are ignoring the stream hash for this object, use StreamableObject.stream_hash
-        begin
+#        begin
           begin
             opts[opt] = opts[opt].to_stream_hash
           rescue NoMethodError
             raise "The model #{opts[opt].class.name} does not have a method called #{opts[opt].activity_attr}. Perhaps you should look at your ActsAsStream configuration, or set :activity_attr"
           end
-        rescue
-          raise "The model #{opts[opt].class.name} does not respond to the :to_stream_hash method. Make that happen, or use a custom packager"
-        end
+#        rescue NoMethodError
+#          raise "The model #{opts[opt].class.name} does not respond to the :to_stream_hash method. Make that happen, or use a custom packager"
+#        end
       end
       # then, if everything is fine, bundle it up into a JSON string
       opts.to_json
@@ -28,21 +28,9 @@ module ActsAsStream
       package = JSON.parse(package)
       package.keys.each{|k| package[k.to_sym] = package[k]; package.delete(k)}
       # Try to cast :who and :object to instances
-      begin
-        package[:who].keys.each{|k| package[:who] = k.titleize.constantize.find(package[:who][k]["id"].to_i)}
-      rescue
-        raise "Cannot translate :who into an instantiated model. Perhaps the model used as the creator of this activity did not have a :to_stream_hash method? The :who value is:\n #{package[:who]}"
-      end
-      begin
-        package[:object].keys.each{|k| package[:object] = k.titleize.constantize.find(package[:object][k]["id"].to_i)}
-      rescue
-        raise "Cannot translate :object into an instantiated model. Perhaps the model used as the :object in this activity did not have a :to_stream_hash method? The :object value is:\n #{package[:object]}"
-      end
-      begin
-        package[:time] = Time.at package[:time].to_i
-      rescue
-        raise "Cannot translate Time given :time value in package of #{package[:time]}"
-      end
+      package[:who].keys.each{|k| package[:who] = k.titleize.constantize.find(package[:who][k]["id"].to_i)}
+      package[:object].keys.each{|k| package[:object] = k.titleize.constantize.find(package[:object][k]["id"].to_i)}
+      package[:time] = Time.at package[:time].to_i
       package
     end
 
